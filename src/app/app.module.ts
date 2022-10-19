b92df22be1b5bcefddd9353ee8a6a65030dddc8a
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-20 03:30:53 +0530

    view page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddlibraryComponent } from './addlibrary/addlibrary.component';
+import { ViewlibraryComponent } from './viewlibrary/viewlibrary.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const appRoutes:Routes=[
+  {
+    path:"",component:AddlibraryComponent
+  },
+  {
+    path:"view",component:ViewlibraryComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AddlibraryComponent
+    AddlibraryComponent,
+    ViewlibraryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/viewlibrary/viewlibrary.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewlibrary',
+  templateUrl: './viewlibrary.component.html',
+  styleUrls: ['./viewlibrary.component.css']
+})
+export class ViewlibraryComponent implements OnInit {
+
+  constructor() { }
+
+  libraryData:any=[]
+
+  ngOnInit(): void {
+  }
+
+}
